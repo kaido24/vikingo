62032962ef30cf62008cdb899516ca3329e7836c
vikingo.se <vhost36791ssh@navi.elkdata.ee>
2014-10-01 14:06:23 +0000

    add page file and change it a bit

--- template.php
+++ template.php
@@ -3,4 +3,19 @@
 /**
  * @file
  * template.php
- */
+*/
+
+function vikingo_form_alter(&$form, &$form_state, $form_id){
+  // Add to cart form overrides
+  if (substr($form_id, 0, 30) == 'commerce_cart_add_to_cart_form'){
+    //kpr($form);
+    $form['attributes']['field_door_color']['#prefix'] = '<div class="col-sm-4">';
+    $form['attributes']['field_door_color']['#suffix'] = '</div>';
+
+    $form['attributes']['field_wall_color']['#prefix'] = '<div class="col-sm-4">';
+    $form['attributes']['field_wall_color']['#suffix'] = '</div>';
+
+    $form['attributes']['field_roof_color']['#prefix'] = '<div class="col-sm-4">';
+    $form['attributes']['field_roof_color']['#suffix'] = '</div>';
+  }
+}
